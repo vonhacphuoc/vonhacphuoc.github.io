451079220f76b48d7bff11127663d8725f1940bf
Võ Nhạc Phước <vonhacphuoc@gmail.com>
2026-06-29 21:40:39 +0700

    ads

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://kqmvhtuhneklsgwfaeds.supabase.co';
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'sb_publishable_pfY2j_oI_crA3IUzTjGGZA_koB9SnZP';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/SuuTapChart/',
+  base: '/',
   plugins: [
     tailwindcss(),
     react()
   ],
 })
+
